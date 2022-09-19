8a3a4f80d02a47fc8d4036d9d4accd663b14bc84
Mahnabbe <datenenaampje@gmail.com>
2022-09-19 11:57:36 +0200

    Remove existing events and create event module handler

--- BiemelBot.js
+++ BiemelBot.js
@@ -11,11 +11,7 @@ const client = new Client({
   ],
 });
 
-client.once("ready", () => {
-  console.log("BiemelBot started and ready!");
-});
-
-//#region Command Handler
+//#region Register Commands
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "Commands");
 const commandFiles = fs
@@ -27,24 +23,23 @@ for (const file of commandFiles) {
   const command = require(filePath);
   client.commands.set(command.data.name, command);
 }
+//#endregion
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+//#region Event Handler
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 //#endregion
 
 client.login(process.env.TOKEN);
