02cf8e07f716860c2af8401f50a96ec37c767e75
Mahnabbe <datenenaampje@gmail.com>
2022-09-28 09:39:42 +0200

    Add user existing check at pr-create

--- Modules/pr-stats.js
+++ Modules/pr-stats.js
@@ -1,9 +1,18 @@
 const PR_Schema = require("../DB-Schemas/pr-schema");
 
 module.exports.createProfile = async (interaction) => {
-  const userID = interaction.user.id;
-
   await interaction.deferReply({ ephemeral: true });
+
+  const userID = interaction.user.id;
+  const user = await PR_Schema.findOne({ userID: userID }).exec();
+
+  if (user != null) {
+    await interaction.editReply({
+      content: "Deze gebruiker bestaat al",
+      ephemeral: true,
+    });
+    return;
+  }
 
   await new PR_Schema({
     userID: userID,
