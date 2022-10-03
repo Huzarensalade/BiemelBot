f025a5d18a2a2b58b2340850fd3b637f45ad530c
Mathijs Nabbe <42406764+Huzarensalade@users.noreply.github.com>
2022-10-03 10:35:04 +0200

    Merge main with Command Handler (#11)
    * Edit README.MD for Command Handler (#2)
    
    * Base Functionality
    
    * Revamped Command Handler with variable listener
    
    * Update README.md
    
    * Update README.md
    
    * Fixed Typo's in command handler (#3)
    
    * Base Functionality
    
    * Revamped Command Handler with variable listener
    
    * Update README.md
    
    * Update README.md
    
    * Fix Typo's
    
    * Fixed more typos for command handler module (#4)
    
    * Base Functionality
    
    * Revamped Command Handler with variable listener
    
    * Update README.md
    
    * Update README.md
    
    * Fix Typo's
    
    * Fixed more typos
    
    * Module mongo db connection (#5)
    
    * Add new dependencies
    
    * Add connection to base file
    
    * Adjust README.md
    
    * Module event handler (#6)
    
    * Transfer existing events to new modules
    
    * Remove existing events and create event module handler
    
    * Module event handler (#7)
    
    * Transfer existing events to new modules
    
    * Remove existing events and create event module handler
    
    * Fix Typo's
    
    * Create PR Module Basics (#8)
    
    * Create Basic Schema
    
    * Create Profile Command
    
    * Add user existing check at pr-create
    
    * Add delete command
    
    * Add "Add" command
    
    * Removed unused code at "PR Add"
    
    * Added Show command
    
    * Update pr-schema.js
    
    * Core updates (#9)
    
    * Update package-lock.json
    
    * Update package.json
    
    * Create Boot Handler (#10)

--- BiemelBot.js
+++ BiemelBot.js
@@ -2,6 +2,7 @@ const { Client, GatewayIntentBits, Collection } = require("discord.js");
 require("dotenv/config");
 const fs = require("fs");
 const path = require("node:path");
+const mongoose = require("mongoose");
 
 const client = new Client({
   intents: [
@@ -11,11 +12,7 @@ const client = new Client({
   ],
 });
 
-client.once("ready", () => {
-  console.log("BiemelBot started and ready!");
-});
-
-//#region Command Handler
+//#region Register Commands
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "Commands");
 const commandFiles = fs
@@ -27,24 +24,23 @@ for (const file of commandFiles) {
   const command = require(filePath);
   client.commands.set(command.data.name, command);
 }
+//#endregion
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+//#region Event Handler
+const eventsPath = path.join(__dirname, "Events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 //#endregion
 
 client.login(process.env.TOKEN);
