ceb530210237dc4f418190373ddf15f7d050e0dd
Mathijs Nabbe <42406764+Huzarensalade@users.noreply.github.com>
2022-10-03 11:14:46 +0200

    Automate Register Commands (#14)
    * Base Functionality
    
    * Revamped Command Handler with variable listener
    
    * Update README.md
    
    * Update README.md
    
    * Fix Typo's
    
    * Fixed more typos
    
    * Merge main with Command Handler (#11)
    
    * Edit README.MD for Command Handler (#2)
    
    * Base Functionality
    
    * Revamped Command Handler with variable listener
    
    * Update README.md
    
    * Update README.md
    
    * Fixed Typo's in command handler (#3)
    
    * Base Functionality
    
    * Revamped Command Handler with variable listener
    
    * Update README.md
    
    * Update README.md
    
    * Fix Typo's
    
    * Fixed more typos for command handler module (#4)
    
    * Base Functionality
    
    * Revamped Command Handler with variable listener
    
    * Update README.md
    
    * Update README.md
    
    * Fix Typo's
    
    * Fixed more typos
    
    * Module mongo db connection (#5)
    
    * Add new dependencies
    
    * Add connection to base file
    
    * Adjust README.md
    
    * Module event handler (#6)
    
    * Transfer existing events to new modules
    
    * Remove existing events and create event module handler
    
    * Module event handler (#7)
    
    * Transfer existing events to new modules
    
    * Remove existing events and create event module handler
    
    * Fix Typo's
    
    * Create PR Module Basics (#8)
    
    * Create Basic Schema
    
    * Create Profile Command
    
    * Add user existing check at pr-create
    
    * Add delete command
    
    * Add "Add" command
    
    * Removed unused code at "PR Add"
    
    * Added Show command
    
    * Update pr-schema.js
    
    * Core updates (#9)
    
    * Update package-lock.json
    
    * Update package.json
    
    * Create Boot Handler (#10)
    
    * Automate Register Commands
    
    * Delete deploy-commands.js

--- BiemelBot.js
+++ BiemelBot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv/config");
 const fs = require("fs");
 const path = require("node:path");
@@ -10,20 +10,6 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-
-//#region Register Commands
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, "Commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
-
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
-}
-//#endregion
 
 //#region Event Handler
 const eventsPath = path.join(__dirname, "Events");
---
+++ Boot/loadCommands.js
@@ -0,0 +1,62 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const { REST } = require("@discordjs/rest");
+const { Routes, Collection } = require("discord.js");
+require("dotenv/config");
+
+module.exports = {
+  name: "loadCommands",
+  async execute(client) {
+    registerCommands(client);
+  },
+};
+
+async function registerCommands(client) {
+  const commands = [];
+  const commandsPath = path.join(process.cwd(), "Commands");
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+  }
+
+  const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+  rest
+    .put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENTID,
+        process.env.GUILDID
+      ),
+      { body: commands }
+    )
+    .then(
+      (data) =>
+        console.log(
+          `Successfully registered ${data.length} application commands.`
+        ),
+
+      loadCommands(client)
+    )
+    .catch(console.error);
+}
+
+async function loadCommands(client) {
+  client.commands = new Collection();
+  const commandsPath = path.join(process.cwd(), "Commands");
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+  }
+
+  console.log("Succesfully loaded all commands!");
+}
