5eaf4516d560676f0d11f417b7ba3aae75774b28
Mathijs Nabbe <42406764+Huzarensalade@users.noreply.github.com>
2022-10-03 10:30:11 +0200

    Create Boot Handler (#10)

--- Events/ready.js
+++ Events/ready.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv/config");
+const path = require("node:path");
+const fs = require("fs");
 
 module.exports = {
   name: "ready",
@@ -12,6 +14,18 @@ module.exports = {
         keepAlive: true,
       }
     );
+    //#region Event Handler
+    const bootPath = path.join(process.cwd(), "Boot");
+    const bootFiles = fs
+      .readdirSync(bootPath)
+      .filter((file) => file.endsWith(".js"));
+
+    for (const file of bootFiles) {
+      const filePath = path.join(bootPath, file);
+      const boot = require(filePath);
+      await boot.execute(client);
+    }
+    // #endregion
 
     console.log("BiemelBot startup succesfull!");
   },
