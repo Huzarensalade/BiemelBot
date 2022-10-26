e24f6dbb7decf5b57f889d6fd65d0f55f8d38b36
Mathijs Nabbe <datenenaampje@gmail.com>
2022-10-26 15:17:55 +0200

    Fixed flow & setup DM

--- Modules/TextToSpeech.js
+++ Modules/TextToSpeech.js
@@ -1,10 +1,6 @@
 const { ActionRowBuilder, SelectMenuBuilder } = require("@discordjs/builders");
 
 module.exports.startProcess = async (interaction) => {
-  const languageID = getLanguageID(interaction);
-};
-
-async function getLanguageID(interaction) {
   await interaction.deferReply({ ephemeral: true });
 
   const languageSelector = new ActionRowBuilder().addComponents(
@@ -30,9 +26,9 @@ async function getLanguageID(interaction) {
     components: [languageSelector],
   });
 
-  let languageID = "";
+  let languageID;
 
-  const collector = message.createMessageComponentCollector({ time: 15000 });
+  const collector = message.createMessageComponentCollector();
   collector.on("collect", (i) => {
     if (
       i.user.id === interaction.user.id &&
@@ -42,17 +38,34 @@ async function getLanguageID(interaction) {
       languageID = i.values[0];
       if (languageID == "nl") {
         interaction.editReply({
-          content: `Gekozen taal: **Nederlands**`,
+          content: `Gekozen taal: **Nederlands** \n\n Nieuwe TTS sessie geopend in je DM's!`,
           components: [],
         });
       } else if (languageID == "en") {
         interaction.editReply({
-          content: `Chosen language: **English**`,
+          content: `Chosen language: **English** \n\n New TTS session started in your DM's!`,
           components: [],
         });
       }
 
-      return languageID;
+      setupCollector(interaction, languageID);
+      collector.stop();
     }
   });
+};
+
+async function setupCollector(interaction, languageID) {
+  let startingMessage;
+
+  if (languageID == "nl") {
+    startingMessage = await interaction.user.send(
+      '**Text To Speech sessie gestart!** \n Typ je berichten hier en de bot zal ze uitspreken in je huidige voicekanaal! \n\n Stop deze sessie met **"/stop"**'
+    );
+  } else if (languageID == "en") {
+    startingMessage = await interaction.user.send(
+      '**Text To Speech session started!** \n Type your message and the bot will say it out loud in your current voicechannel! \n\n Stop this session by typing **"/stop"**'
+    );
+  }
+
+  
 }
